e81bc42add9b992b27ad08a1a7a68734b8f3809f
trasea986 <trasea986@hotmail.com>
2022-02-23 18:26:47 -0800

    repro to equal area in meters

--- src/script_s1.R
+++ src/script_s1.R
@@ -25,6 +25,15 @@ points_final <- spTransform(coord.19, crs(cdl_ID.WA.19))
 plot(cdl_ID.WA.19)
 points(points_final, pch = 1, col = "blue", cex = 1) ####gives the map with points overlayed (map pasted below)
 
+#reproject layers and points
+#set projection as USA Contiguous Albers Equal Area Conic
+projection <- "ESRI:102003"
+points_final <- spTransform(points_final, projection)
+cdl_ID.WA.19 <- projectRaster(cdl_ID.WA.19, crs = projection, method = "ngb")
+
+#plotting to check reprojection
+plot(cdl_ID.WA.19)
+points(points_final, pch = 1, col = "blue", cex = 1)
 
 #added progress, reduced size
-sampled_area <- sample_lsm(cdl_ID.WA.19, y = points_final, size = .001, shape = "circle", what = "lsm_l_shdi", progress = TRUE)
+sampled_area <- sample_lsm(cdl_ID.WA.19, y = points_final, size = 500, shape = "circle", what = "lsm_l_shdi", progress = TRUE)
